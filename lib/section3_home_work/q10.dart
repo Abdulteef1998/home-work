f1a75ec8c38032ef771d55ea9ddd412d9b3e6b0d
AbdulteefRashid <abdulte97@gmail.com>
2025-08-19 14:33:20 +0300

    Slove Q10

---
+++ lib/section3_home_work/q10.dart
@@ -0,0 +1,24 @@
+// Q10
+// a) Demonstrate var vs dynamic: assign dynamic value first as an int, then as a String, printing
+// after each.
+// b) Create var greeting = 'Hi'; change it to another String and print.
+// c) Declare num pi = 3.14159; print pi.toInt() and pi.toStringAsFixed(3)
+
+void main() {
+  var age = 26;
+  print(age);
+  age =
+      'Twenty Six'; // canot assign a String to int varrible by var  . I left it like this to prove the value cannot change and printed
+  print(age);
+  dynamic yourAge = 20;
+  print(yourAge);
+  yourAge = 'Twenty';
+  print(yourAge);
+  var text = 'Hi';
+  print(text);
+  text = 'Hello';
+  print(text);
+  num pi = 3.14159;
+  print(pi.toInt());
+  print(pi.toStringAsFixed(3));
+}
